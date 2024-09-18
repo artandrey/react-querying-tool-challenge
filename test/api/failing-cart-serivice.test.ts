847fcb8eb4e023c151702a8c20131e1c77d2c21b
Artandrey <artandrey7777@gmail.com>
2024-09-18 22:52:35 +0300

    refactor: move MockFailingCartService initialization to beforeEach hook

--- test/api/failing-cart-serivice.test.ts
+++ test/api/failing-cart-serivice.test.ts
@@ -1,18 +1,24 @@
-import { MockFailingCartService } from '../../src/shared/api/cart-service';
+import {
+    ICartService,
+    MockFailingCartService,
+} from '../../src/shared/api/cart-service';
 
 describe('MockFailingCartService service', () => {
+    let cartService: ICartService;
+
+    beforeEach(() => {
+        cartService = new MockFailingCartService();
+    });
+
     test('should be defined', () => {
-        const cartService = new MockFailingCartService();
         expect(cartService).toBeDefined();
     });
 
     test('should fail give products on first try', () => {
-        const cartService = new MockFailingCartService();
         expect(() => cartService.getProductsInCart()).rejects.toThrow();
     });
 
     test('should return products on second try', async () => {
-        const cartService = new MockFailingCartService();
         await expect(() => cartService.getProductsInCart()).rejects.toThrow();
 
         await expect(cartService.getProductsInCart()).resolves.toStrictEqual(
