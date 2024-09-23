670225b06daa6c18f13fc8327f438e195e451cc8
Artandrey <artandrey7777@gmail.com>
2024-09-23 23:00:26 +0300

    feat(react-query): implement optimistic update for cart

--- src/feat/add-product/lib/hooks/use-add-product.ts
+++ src/feat/add-product/lib/hooks/use-add-product.ts
@@ -7,15 +7,44 @@ export type UseAddProductResult = {
     execute: (title: string) => Promise<void>;
 };
 
+export type OptimisticProduct = Product & { isOptimistic?: boolean };
+
 export const useAddProduct = (): UseAddProductResult => {
     const queryClient = useQueryClient();
 
     const { mutateAsync, isPending } = useMutation({
         mutationFn: (title: string) => api().cart.addProductToCart(title),
         onSuccess: (data) =>
-            queryClient.setQueryData<Readonly<Product[]>>(['cart'], (old) =>
-                old ? [...old, data] : undefined
+            queryClient.setQueryData<Readonly<OptimisticProduct[]>>(
+                ['cart'],
+                (old) =>
+                    old && [
+                        ...old.filter((product) => !product.isOptimistic),
+                        data,
+                    ]
             ),
+        onMutate: (title: string) => {
+            const id = Date.now();
+            queryClient.setQueryData<Readonly<OptimisticProduct[]>>(
+                ['cart'],
+                (old) =>
+                    old && [
+                        ...old,
+                        {
+                            id,
+                            title,
+                            isOptimistic: true,
+                        },
+                    ]
+            );
+            return id;
+        },
+        onError: (error, title, id) => {
+            queryClient.setQueryData<Readonly<OptimisticProduct[]>>(
+                ['cart'],
+                (old) => old?.filter((product) => product.id !== id)
+            );
+        },
     });
 
     const execute = async (title: string) => {
