5d30b5460d6b82fce415525d82e703017e627977
Artandrey <artandrey7777@gmail.com>
2024-10-17 00:33:32 +0300

    refactor: update formatting

--- src/feat/add-product/lib/hooks/use-add-product.ts
+++ src/feat/add-product/lib/hooks/use-add-product.ts
@@ -3,54 +3,50 @@ import { api } from '../../../../shared/api';
 import { Product } from '../../../../shared/api/cart-service';
 
 export type UseAddProductResult = {
-    isLoading: boolean;
-    execute: (title: string) => Promise<void>;
+  isLoading: boolean;
+  execute: (title: string) => Promise<void>;
 };
 
 export type OptimisticProduct = Product & { isOptimistic?: boolean };
 
 export const useAddProduct = (): UseAddProductResult => {
-    const queryClient = useQueryClient();
+  const queryClient = useQueryClient();
 
-    const { mutateAsync, isPending } = useMutation({
-        mutationFn: (title: string) => api().cart.addProductToCart(title),
-        retry: 2,
-        onSuccess: (data) =>
-            queryClient.setQueryData<Readonly<OptimisticProduct[]>>(
-                ['cart'],
-                (old) =>
-                    old && [
-                        ...old.filter((product) => !product.isOptimistic),
-                        data,
-                    ]
-            ),
-        onMutate: (title: string) => {
-            const id = Date.now();
-            queryClient.setQueryData<Readonly<OptimisticProduct[]>>(
-                ['cart'],
-                (old) =>
-                    old && [
-                        ...old,
-                        {
-                            id,
-                            title,
-                            isOptimistic: true,
-                        },
-                    ]
-            );
-            return id;
-        },
-        onError: (error, title, id) => {
-            queryClient.setQueryData<Readonly<OptimisticProduct[]>>(
-                ['cart'],
-                (old) => old?.filter((product) => product.id !== id)
-            );
-        },
-    });
+  const { mutateAsync, isPending } = useMutation({
+    mutationFn: (title: string) => api().cart.addProductToCart(title),
+    retry: 2,
+    onSuccess: (data) =>
+      queryClient.setQueryData<Readonly<OptimisticProduct[]>>(
+        ['cart'],
+        (old) =>
+          old && [...old.filter((product) => !product.isOptimistic), data]
+      ),
+    onMutate: (title: string) => {
+      const id = Date.now();
+      queryClient.setQueryData<Readonly<OptimisticProduct[]>>(
+        ['cart'],
+        (old) =>
+          old && [
+            ...old,
+            {
+              id,
+              title,
+              isOptimistic: true,
+            },
+          ]
+      );
+      return id;
+    },
+    onError: (_error, _title, id) => {
+      queryClient.setQueryData<Readonly<OptimisticProduct[]>>(['cart'], (old) =>
+        old?.filter((product) => product.id !== id)
+      );
+    },
+  });
 
-    const execute = async (title: string) => {
-        await mutateAsync(title);
-    };
+  const execute = async (title: string) => {
+    await mutateAsync(title);
+  };
 
-    return { isLoading: isPending, execute };
+  return { isLoading: isPending, execute };
 };
