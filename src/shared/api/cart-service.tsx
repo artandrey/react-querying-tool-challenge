c15b15945c94c52d3cc26df226ef6b2fdf3db478
Artandrey <artandrey7777@gmail.com>
2024-10-16 22:57:01 +0300

    feat: implement getAvailableProducts method, add product renewal after timeout

--- src/shared/api/cart-service.tsx
+++ src/shared/api/cart-service.tsx
@@ -1,48 +1,73 @@
 export class Product {
-    constructor(public id: number, public title: string) {}
+  constructor(public id: number, public title: string) {}
 }
 
 export interface ICartService {
-    getProductsInCart(): Promise<Readonly<Product[]>>;
-    addProductToCart(title: string): Promise<Readonly<Product>>;
+  getProductsInCart(): Promise<Readonly<Product[]>>;
+  addProductToCart(title: string): Promise<Readonly<Product>>;
+  getAvailableProducts(): Promise<Readonly<Product[]>>;
 }
 
 export class MockFailingCartService implements ICartService {
-    private productId = 0;
-    private shouldFail = true;
-    private readonly productsInCart: Product[] = [];
+  private productId = 0;
+  private shouldFail = true;
+  private readonly productsInCart: Product[] = [];
+  private availableProducts: Product[] = [
+    new Product(this.generateId(), 'Product A'),
+    new Product(this.generateId(), 'Product B'),
+    new Product(this.generateId(), 'Product C'),
+  ];
 
-    public async getProductsInCart(): Promise<Readonly<Product[]>> {
-        await this.wait(400);
-        this.maybeFail();
-        return this.productsInCart;
-    }
+  public async getProductsInCart(): Promise<Readonly<Product[]>> {
+    await this.wait(400);
+    this.maybeFail();
+    return this.productsInCart;
+  }
 
-    public async addProductToCart(title: string): Promise<Readonly<Product>> {
-        await this.wait(200);
-        this.maybeFail();
-        const product = new Product(this.generateId(), title);
-        this.productsInCart.push(product);
-        return product;
+  public async addProductToCart(title: string): Promise<Readonly<Product>> {
+    await this.wait(200);
+    this.maybeFail();
+    const productIndex = this.availableProducts.findIndex(
+      (p) => p.title === title
+    );
+    if (productIndex === -1) {
+      throw new Error('Product not available');
     }
+    const product = this.availableProducts.splice(productIndex, 1)[0];
+    this.productsInCart.push(product);
+    this.scheduleProductReplenishment(title);
+    return product;
+  }
 
-    private generateId() {
-        return this.productId++;
-    }
+  public async getAvailableProducts(): Promise<Readonly<Product[]>> {
+    await this.wait(300);
+    this.maybeFail();
+    return this.availableProducts;
+  }
 
-    private wait(time: number = 1000): Promise<void> {
-        return new Promise((resolve) => {
-            setTimeout(resolve, time);
-        });
-    }
+  private generateId() {
+    return this.productId++;
+  }
 
-    private maybeFail() {
-        if (this.shouldFail) {
-            this.shouldFail = false;
-            console.error('Cart action failed');
-            throw new Error('Cart action failed');
-        } else {
-            this.shouldFail = true;
-        }
+  private wait(time: number = 1000): Promise<void> {
+    return new Promise((resolve) => {
+      setTimeout(resolve, time);
+    });
+  }
+
+  private maybeFail() {
+    if (this.shouldFail) {
+      this.shouldFail = false;
+      console.error('Cart action failed');
+      throw new Error('Cart action failed');
+    } else {
+      this.shouldFail = true;
     }
+  }
+
+  private scheduleProductReplenishment(title: string) {
+    setTimeout(() => {
+      this.availableProducts.push(new Product(this.generateId(), title));
+    }, 3000);
+  }
 }
--- test/api/failing-cart-serivice.test.ts
+++ test/api/failing-cart-serivice.test.ts
@@ -1,40 +1,91 @@
 import {
-    ICartService,
-    MockFailingCartService,
+  ICartService,
+  MockFailingCartService,
 } from '../../src/shared/api/cart-service';
 
-describe.skip('MockFailingCartService service', () => {
-    let cartService: ICartService;
+describe('MockFailingCartService', () => {
+  let cartService: ICartService;
 
-    beforeEach(() => {
-        cartService = new MockFailingCartService();
-    });
+  beforeEach(() => {
+    cartService = new MockFailingCartService();
+  });
 
-    test('should be defined', () => {
-        expect(cartService).toBeDefined();
-    });
+  test('should be defined', () => {
+    expect(cartService).toBeDefined();
+  });
 
-    test('should fail give products on first try', () => {
-        expect(() => cartService.getProductsInCart()).rejects.toThrow();
-    });
+  test('should fail to get products in cart on first try', () => {
+    expect(() => cartService.getProductsInCart()).rejects.toThrow();
+  });
 
-    test('should return products on second try', async () => {
-        await expect(() => cartService.getProductsInCart()).rejects.toThrow();
+  test('should return products in cart on second try', async () => {
+    await expect(() => cartService.getProductsInCart()).rejects.toThrow();
+    await expect(cartService.getProductsInCart()).resolves.toEqual([]);
+  });
 
-        await expect(cartService.getProductsInCart()).resolves.toEqual([]);
-    });
+  test('should fail to add product on first try', () => {
+    expect(() => cartService.addProductToCart('Product A')).rejects.toThrow();
+  });
 
-    test('should fail to add product on first try', () => {
-        expect(() => cartService.addProductToCart('test')).rejects.toThrow();
-    });
+  test('should add product on second try', async () => {
+    await expect(() =>
+      cartService.addProductToCart('Product A')
+    ).rejects.toThrow();
+    await expect(cartService.addProductToCart('Product A')).resolves.toEqual(
+      expect.objectContaining({ title: 'Product A' })
+    );
+  });
 
-    test('should add product on second try', async () => {
-        await expect(() =>
-            cartService.addProductToCart('test')
-        ).rejects.toThrow();
+  test('should fail to get available products on first try', () => {
+    expect(() => cartService.getAvailableProducts()).rejects.toThrow();
+  });
 
-        await expect(cartService.addProductToCart('test')).resolves.toEqual(
-            expect.objectContaining({ title: 'test' })
-        );
-    });
+  test('should return available products on second try', async () => {
+    await expect(() => cartService.getAvailableProducts()).rejects.toThrow();
+    const products = await cartService.getAvailableProducts();
+    expect(products).toHaveLength(3);
+    expect(products[0].title).toBe('Product A');
+    expect(products[1].title).toBe('Product B');
+    expect(products[2].title).toBe('Product C');
+  });
+
+  test('should remove product from available products when added to cart', async () => {
+    await expect(cartService.getAvailableProducts()).rejects.toThrow();
+    await expect(cartService.addProductToCart('Product A')).resolves.toEqual(
+      expect.objectContaining({ title: 'Product A' })
+    );
+    await expect(() => cartService.getAvailableProducts()).rejects.toThrow();
+
+    const availableProducts = await cartService.getAvailableProducts();
+    expect(availableProducts).toHaveLength(2);
+    expect(
+      availableProducts.find((p) => p.title === 'Product A')
+    ).toBeUndefined();
+  });
+
+  test('should replenish product after being added to cart', async () => {
+    vi.useFakeTimers({ shouldAdvanceTime: true });
+    await expect(cartService.getAvailableProducts()).rejects.toThrow();
+    const initialProducts = await cartService.getAvailableProducts();
+    expect(initialProducts).toHaveLength(3);
+    await expect(cartService.addProductToCart('Product A')).rejects.toThrow();
+    await cartService.addProductToCart('Product A');
+    await expect(cartService.getAvailableProducts()).rejects.toThrow();
+    let availableProducts = await cartService.getAvailableProducts();
+    expect(availableProducts).toHaveLength(2);
+
+    await expect(cartService.getAvailableProducts()).rejects.toThrow();
+
+    await vi.advanceTimersByTimeAsync(3000);
+
+    availableProducts = await cartService.getAvailableProducts();
+
+    expect(availableProducts).toHaveLength(3);
+
+    const replenishedProduct = availableProducts.find(
+      (p) => p.title === 'Product A'
+    );
+    expect(replenishedProduct).toBeDefined();
+    expect(replenishedProduct?.id).not.toBe(0);
+  });
 });
